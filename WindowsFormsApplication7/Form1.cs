3b69e6dfed8727326bdeec23aef1cfb6acc14be6
Tzr-black <377191154@qq.com>
2018-05-22 15:59:57 +0800

    6

--- WindowsFormsApplication7/Form1.cs
+++ WindowsFormsApplication7/Form1.cs
@@ -20,5 +20,23 @@ namespace WindowsFormsApplication7
         {
 
         }
+
+        private void textEdit4_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            string x1 = textX1.Text;
+            string x2 = textX2.Text;
+            string x3 = textX3.Text;
+            string x4 = textX4.Text;
+            string x5 = textX5.Text;
+            string x6 = textX6.Text;
+            string x7 = textX7.Text;
+            double[,] w1 = new double[,] { {0.9880,-0.2664,-0.9157,0.0206,-0.5388,-0.7711,2.4399},
+                                           {3.0299,2.1328,-1.6592,-0.5420,-0.1957,1.4950,-0.2351} };
+        }
     }
 }
